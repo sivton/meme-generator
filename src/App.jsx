40d0ae3d49fc1cc26bc7e2efb8ed8d04f3748878
vis <48868398+vis2x@users.noreply.github.com>
2023-01-22 02:43:40 +0000

    Added meme button and inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./index.css";
 import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 export default function App() {
   return (
     <div className="App">
         <Header />
+        <Meme />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "../index.css";
 export default function Header() {
     return (          
         <header className="header--container">
-            <img className="header--image" src="../images/troll-face.png"></img>
+            <img className="header--image" src="https://pngimg.com/d/trollface_PNG1.png"/>
             <h2 className="header--title">Header component</h2>
             <h4 className="header--project">Meme Generator</h4>
         </header>
