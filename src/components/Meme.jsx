b8ea519bc20f60b1ac1d24dea5b50d719864f0d7
vis <48868398+vis2x@users.noreply.github.com>
2023-01-25 01:14:32 +0000

    Added images to meme generator

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,12 +1,24 @@
 import React from "react"
-import "../index.css";
+import memesData from "../memesData"
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function getMemeImage() {
+        let memesArray = memesData.data.memes;
+        let randomNum = Math.floor(Math.random() * memesArray.length);
+        let url = memesArray[randomNum].url;
+        setMemeImage(memesArray[randomNum].url)
+    }
+
+
     return (
-        <form className="meme--container">
+        <div className="meme--container">
             <input type="text" className="meme--input" placeholder="Enter top text" />
             <input type="text" className="meme--input" placeholder="Enter bottom text" />
-            <button className="meme--button">Generate new meme image</button>
-        </form>
+            <button className="meme--button" onClick={getMemeImage}>Generate new meme image</button>
+            <img className="meme--image" src={memeImage}/>
+        </div>
     )
 }
