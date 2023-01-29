b7e1508682d3153113f3519598deafa543c9b94f
vis <48868398+vis2x@users.noreply.github.com>
2023-01-29 02:11:26 +0000

    Added text to memes

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react"
+import React from "react"
 import memesData from "../memesData"
 
 export default function Meme() {
 
-    const [meme, setMeme] = useState({
+    const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeImages, setMemeImage] = useState(memesData)
+    const [allMemeImages, setMemeImage] = React.useState(memesData)
 
 
     function getMemeImage() {
-        let memesArray = allMemeImages.data.memes;
-        let randomNum = Math.floor(Math.random() * memesArray.length);
-        let url = memesArray[randomNum].url;
+        const memesArray = allMemeImages.data.memes;
+        const randomNum = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNum].url;
 
         setMeme(oldMeme => {
             return ({
@@ -25,13 +25,47 @@ export default function Meme() {
         })
     }
 
+    function setMemeText(event) {
+        const { name, value } = event.target
+        setMeme(oldMeme => ({
+            ...oldMeme,
+            [name]: value
+        }))
+    }
+
 
     return (
         <div className="meme--container">
-            <input type="text" className="meme--input" placeholder="Enter top text" />
-            <input type="text" className="meme--input" placeholder="Enter bottom text" />
-            <button className="meme--button" onClick={getMemeImage}>Generate new meme image</button>
-            <img className="meme--image" src={meme.randomImage} />
+            <input
+                type="text"
+                className="meme--input"
+                placeholder="Enter top text"
+                name="topText"
+                value={meme.topText}
+                onChange={setMemeText}
+            />
+            <input
+                type="text"
+                className="meme--input"
+                placeholder="Enter bottom text"
+                name="bottomText"
+                value={meme.bottomText}
+                onChange={setMemeText}
+            />
+            <button
+                className="meme--button"
+                onClick={getMemeImage}>
+                Generate new meme image
+            </button>
+
+            <div className="meme">
+                <img
+                    className="meme--image"
+                    src={meme.randomImage}
+                />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </div>
     )
 }
